e74652b99e696ac2503afc8b6b220fdde9219cac
dlawlsdud0419 <59009654+dlawlsdud0419@users.noreply.github.com>
2020-11-04 16:19:53 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,19 +2,19 @@
 function init(){
     setTimeout(function() {
         document.getElementById("loading").classList.add("isFinish")
-      }, 3000);
+      }, 5000);
       setTimeout(function() {
         document.getElementById("loading").remove()
-      }, 4000);
+      }, 6000);
      setTimeout(function() {
         document.getElementById("overlay").classList.add("is-open")
         document.getElementById("canvas-bg").classList.add("is-open")
-      }, 4000);
+      }, 6000);
       setTimeout(function() {
         document.getElementById("navbar").style.display ='flex';
         document.getElementById("prev").style.opacity = '1';
         document.getElementById("next").style.opacity = '1';
-      }, 5000);
+      }, 7000);
       //document.getElementById("overlay").classList.add("is-open");
 }
 
