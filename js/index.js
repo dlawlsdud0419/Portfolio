1ff39ad2db231d8209d2daba89994b5d05eed415
dlawlsdud0419 <59009654+dlawlsdud0419@users.noreply.github.com>
2020-11-04 17:25:09 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,19 +2,19 @@
 function init(){
     setTimeout(function() {
         document.getElementById("loading").classList.add("isFinish")
-      }, 5000);
+      }, 6000);
       setTimeout(function() {
         document.getElementById("loading").remove()
-      }, 6000);
+      }, 7000);
      setTimeout(function() {
         document.getElementById("overlay").classList.add("is-open")
         document.getElementById("canvas-bg").classList.add("is-open")
-      }, 6000);
+      }, 7000);
       setTimeout(function() {
         document.getElementById("navbar").style.display ='flex';
         document.getElementById("prev").style.opacity = '1';
         document.getElementById("next").style.opacity = '1';
-      }, 7000);
+      }, 8000);
       //document.getElementById("overlay").classList.add("is-open");
 }
 
